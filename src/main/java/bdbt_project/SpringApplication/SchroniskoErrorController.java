645f87c0d5a34ea673b4e8b29ba4c813451ed760
juziembl <B+34DuV>
2023-01-11 15:50:19 +0100

    add configuration/MVN files | changed minor details

--- src/main/java/bdbt_project/SpringApplication/SchroniskoErrorController.java
+++ src/main/java/bdbt_project/SpringApplication/SchroniskoErrorController.java
@@ -1,7 +1,6 @@
 package bdbt_project.SpringApplication;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
